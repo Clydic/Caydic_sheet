c8d53335b98d20dcc16103cd5a2f9ba16d13bbb9
Cédric Paimblanc <cedric.du.50@gmail.com>
2022-05-05 15:42:30 +0200

    mise en place des routes

---
+++ frontend/services/CompetenceDataService.js
@@ -0,0 +1,24 @@
+import http from "../http-common";
+class CompetenceDataService {
+    getAll() {
+        return http.get("/competences");
+    }
+    get(id) {
+        return http.get(`/competences/${id}`);
+    }
+    create(data) {
+        return http.post("/competences", data);
+    }
+    update() {
+        return http.put(`/competences/${id}`, data);
+    }
+    delete(id) {
+        return http.delete(`/competences/${id}`);
+    }
+    deleteAll() {
+        return http.delete(`/competences`);
+    }
+    findByName(name_competence) {
+        return http.get(`/competences?name_competence=${name_competence}`);
+    }
+}
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,8 +1,24 @@
 import { createWebHistory, createRouter } from "vue-router";
 const routes = [{
-    path: "/",
-    alias: "/competences",
-    name: "competences",
-    component: () =>
-        import ("./components/CompetenceList.vue")
-}]
+        path: "/",
+        alias: "/competences",
+        name: "competences",
+        component: () =>
+            import ("./components/CompetenceList")
+    },
+    {
+        path: "/competences/:id",
+        name: "competences-details",
+        component: () =>
+            import ("./components/Competence")
+    },
+    {
+        path: "/add",
+        name: "add",
+        component: () =>
+            import ("./components/addCompetence")
+    }
+];
+const router = createRouter({
+    history: createWebHistory().routes,
+});
