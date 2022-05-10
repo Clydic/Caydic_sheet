932a81864d44ec8c1ec490c7299ac338d3760055
Cédric Paimblanc <cedric.du.50@gmail.com>
2022-05-10 12:01:20 +0200

    Mise en place du modele arme

---
+++ backend/models/armes.model.js
@@ -0,0 +1,29 @@
+const { Sequelize } = require(".")
+module.exports = (sequelize, Sequelize) => {
+    const Arme = sequelize.define("arme", {
+        id_arme: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        name_arme: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        type_degat: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        category_arme: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        nb_degat: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+
+    });
+    return Arme;
+};
