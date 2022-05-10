8fee00bbf5ec1727932c4706d44c5465129a9204
Cédric Paimblanc <cedric.du.50@gmail.com>
2022-05-10 05:13:30 +0200

    Test de lecture de fichier, déplacement de donnée list_of_carac

---
+++ frontend/src/assets/js/list_of_carac.js
@@ -0,0 +1,37 @@
+var list_of_carac = {
+    for: {
+        nom: "Force",
+        valeur: 17,
+
+    },
+    dex: {
+        nom: "Dextérité",
+        valeur: 10,
+
+    },
+    con: {
+        nom: "Constitution",
+        valeur: 16,
+
+    },
+    sag: {
+        nom: "Intelligence",
+        valeur: 13,
+
+    },
+    int: {
+        nom: "Charisme",
+        valeur: 16,
+
+    },
+    cha: {
+        nom: "Sagesse",
+        valeur: 8,
+
+    },
+    bba: {
+        nom: "Bonus de base à l'attaque",
+        valeur: "12/7/2"
+    }
+}
+module.exports = list_of_carac
--- frontend/src/components/MyCharac.vue
+++ frontend/src/components/MyCharac.vue
@@ -3,6 +3,8 @@
         <h2>{{Name}}</h2>
         <h3>{{age}}</h3>
         <p>{{classe}}</p>
+        <p>Attaque contact : {{attaqueCac}}</p>
+        <p>Attaque dist : {{attaqueDist}}</p>
         <p>
             <button class="btn btn-primary" 
             type="button" data-bs-toggle="collapse" 
@@ -36,39 +38,8 @@
         </div>
 </template>
 <script>
+var list_of_carac = require( "../assets/js/list_of_carac.js");
 var id_js = 0
-var list_of_carac = {
-                for : {
-               nom : "Force",
-               valeur : 17,
-
-                },
-                dex : {
-               nom : "Dextérité",
-               valeur : 10,
-
-                },
-                con : {
-               nom : "Constitution",
-               valeur : 16,
-
-                },
-                sag : {
-               nom : "Intelligence",
-               valeur : 13,
-
-                },
-                int : {
-               nom : "Charisme",
-               valeur : 16,
-
-                },
-                cha : {
-               nom : "Sagesse",
-               valeur : 8,
-
-                },
-}
 export default {
     data(){
         return {
@@ -96,11 +67,6 @@ export default {
                     carac :list_of_carac.sag
                 }
             ],
-            myObject: {
-                title: 'How to do lists in Vue',
-                author: 'Jane Doe',
-                publishedAt: '2016-04-10'
-        },
     }
     },
     methods: {
@@ -114,6 +80,24 @@ export default {
             }
         },
 
+    },
+    computed: {
+        attaqueCac(){
+            let bba = list_of_carac.bba.valeur.split("/");
+            for(let i =0; i<bba.length ; i++){
+              bba[i] = parseInt(bba[i]) + parseInt(this.modif(list_of_carac.for));
+              bba[i] = "+" + bba[i];
+            }
+            return bba.join("/");
+        },
+        attaqueDist(){
+            let bba = list_of_carac.bba.valeur.split("/");
+            for(let i =0; i<bba.length ; i++){
+              bba[i] = parseInt(bba[i]) + parseInt(this.modif(list_of_carac.dex));
+              bba[i] = "+" + bba[i];
+            }
+            return bba.join("/");
+        }
     },
 }
 </script>
